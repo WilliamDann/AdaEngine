f7f1023e581f1c919b334633c4fe50a6f0982664
WilliamDann <s-william.dann@lwtech.edu>
2025-03-24 15:18:41 -0700

    cleanup

--- main.go
+++ main.go
@@ -8,17 +8,7 @@ import (
 
 func main() {
 	pos := game.NewPosition(game.ItalianPosition)
-	// pos.GetBoard().Set(*game.NewPiece(game.Black, game.Pawn), *game.NewCoord(2, 3))
-
 	mg := game.NewMoveGenerator(*pos)
-
-	for i := 0; i < 1_000_000; i++ {
-		mg.Generate()
-	}
-
-	// for _, move := range moves {
-	// 	pos.GetBoard().Set(game.Piece{'x', false}, move.To)
-	// }
 
 	fmt.Println(mg.Generate())
 	fmt.Println(pos.GetBoard())
